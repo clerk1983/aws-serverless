7c5eeb7301a75713e5d2ad57f7611260a10e8e43
CLERK1983 <41065074+clerk1983@users.noreply.github.com>
2024-08-13 20:26:55 +0900

    Treat OpenAPI

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,5 @@
 #!bin/bash
 
-echo ""
-echo "-- OPEN-API S3 SYNC ---------------------"
-echo $(aws s3 sync ./frontend/static/ s3://aws-serverless-frontend-s3-ap-northeast-1-465068362057 --delete)
-echo ""
-
 echo "-- SAM BUILD & DEPLOY ----------"
 sam build
 sam deploy
@@ -13,3 +8,5 @@ echo ""
 echo "-- FRONT-END S3 SYNC ---------------------"
 echo $(aws s3 sync ./frontend/static/ s3://aws-serverless-frontend-s3-ap-northeast-1-465068362057 --delete)
 echo ""
+
+aws apigateway get-export --rest-api-id 8bgmut0ajj --stage-name Prod --export-type oas30 --accepts application/yaml  open-api.yaml
