c348fa852d7a726ec44c2eab41c7da809566ae77
CLERK1983 <41065074+clerk1983@users.noreply.github.com>
2024-08-13 20:35:12 +0900

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -9,4 +9,4 @@ echo "-- FRONT-END S3 SYNC ---------------------"
 echo $(aws s3 sync ./frontend/static/ s3://aws-serverless-frontend-s3-ap-northeast-1-465068362057 --delete)
 echo ""
 
-aws apigateway get-export --rest-api-id 8bgmut0ajj --stage-name Prod --export-type oas30 --accepts application/yaml  open-api.yaml
+aws apigateway get-export --rest-api-id 8bgmut0ajj --stage-name Prod --export-type oas30 --accepts application/yaml  open-api.yaml > /dev/null
