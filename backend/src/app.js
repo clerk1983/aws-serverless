704d3e495520fd64f913f319d89ded77c66884e2
CLERK1983 <41065074+clerk1983@users.noreply.github.com>
2024-08-13 14:07:47 +0900

    [update] CORS

--- backend/src/app.js
+++ backend/src/app.js
@@ -14,22 +14,29 @@ exports.handler = async event => {
 
   try {
     const data = await dynamoDb.send(new GetItemCommand(params))
-
+    const headers = {
+      'Access-Control-Allow-Origin': '*', // 必要に応じてオリジンを指定
+      'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+      'Access-Control-Allow-Headers': 'Content-Type',
+    }
     if (!data.Item) {
       return {
         statusCode: 404,
+        headers,
         body: JSON.stringify({ message: 'Item not found' }),
       }
     }
 
     return {
       statusCode: 200,
+      headers,
       body: JSON.stringify(data.Item),
     }
   } catch (error) {
     console.error(error)
     return {
       statusCode: 500,
+      headers,
       body: JSON.stringify({
         message: 'Could not retrieve item',
         error: error.message,
--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,10 @@
 #!bin/bash
 
+echo "-- SAM BUILD & DEPLOY ----------"
 sam build
 sam deploy
 
+echo ""
+echo "-- S3 SYNC ---------------------"
 echo $(aws s3 sync ./frontend/static/ s3://aws-serverless-frontend-s3-ap-northeast-1-465068362057 --delete)
+echo ""
