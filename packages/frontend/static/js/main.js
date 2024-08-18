35df61ecfc12bd966e8ae807d1491b5ff81cafef
CLERK1983 <41065074+clerk1983@users.noreply.github.com>
2024-08-18 09:34:56 +0900

    Release 1.0.0

--- packages/frontend/static/js/main.js
+++ packages/frontend/static/js/main.js
@@ -4,7 +4,12 @@ const EMPTY = '0';
 const DARK = '1';
 const LIGHT = '2';
 
+const WINNER_DRAW = '0';
+const WINNER_DARK = '1';
+const WINNER_LIGHT = '2';
+
 const nextDiscMessage = document.querySelector('#next-disc-message');
+const warningMessageElement = document.querySelector('#warning-message');
 
 /**
  * 新規ゲームを開始する
@@ -49,7 +54,7 @@ const registerTurn = async (turnCount, disc, x, y) => {
  * 盤面を取得する
  * @param {*} turnCount
  */
-const showBoard = async (turnCount) => {
+const showBoard = async (turnCount, previousDisc) => {
   const gameId = sessionStorage.getItem('gameId');
   const response = await fetch(
     `${API_END_POINT}games/${gameId}/turns/${turnCount}`,
@@ -58,7 +63,12 @@ const showBoard = async (turnCount) => {
   const resBody = await response.json();
   const board = resBody.board;
   const nextDisc = resBody.nextDisc;
+  const winnerDisc = resBody.winnerDisc;
+
+  showWarningMessage(previousDisc, nextDisc, winnerDisc);
+
   showNextDiscMessage(nextDisc);
+
   const boardElement = document.querySelector('#board');
   while (boardElement.firstChild) {
     boardElement.removeChild(boardElement.firstChild);
@@ -84,7 +94,7 @@ const showBoard = async (turnCount) => {
             y,
           );
           if (registerTurnRes.ok) {
-            await showBoard(nextTurnCount);
+            await showBoard(nextTurnCount, nextDisc);
           }
         });
       }
@@ -93,9 +103,39 @@ const showBoard = async (turnCount) => {
   });
 };
 
+const discToString = (disc) => {
+  return disc == DARK ? '黒' : '白';
+};
+
+const showWarningMessage = (previousDisc, nextDisc, winnerDisc) => {
+  const message = warningMessage(previousDisc, nextDisc, winnerDisc);
+  warningMessageElement.innerHTML = message;
+  if (message) {
+    warningMessageElement.style.display = 'block';
+  } else {
+    warningMessageElement.style.display = 'none';
+  }
+};
+
+const warningMessage = (previousDisc, nextDisc, winnerDisc) => {
+  if (nextDisc != null) {
+    if (previousDisc === nextDisc) {
+      const skipped = nextDisc === DARK ? LIGHT : DARK;
+      return `${discToString(skipped)}の番です`;
+    } else {
+      return null;
+    }
+  } else {
+    if (winnerDisc === WINNER_DRAW) {
+      return `引き分けです`;
+    }
+    return `${discToString(winnerDisc)}の勝ちです`;
+  }
+};
+
 const showNextDiscMessage = (nextDisc) => {
   if (nextDisc) {
-    const color = nextDisc == DARK ? '黒' : '白';
+    const color = discToString(nextDisc);
     nextDiscMessage.innerHTML = `次は${color}の番です`;
   } else {
     nextDiscMessage.innerHTML = '';
